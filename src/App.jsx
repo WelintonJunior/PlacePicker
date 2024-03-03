9cfbae54fe9df13d386365c613553fdc8024aadb
WelintonJunior <welintonjr011@gmail.com>
2024-03-03 16:08:40 -0300

    Added a localStorage to record the selected places

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,12 @@ function App() {
   const [pickedPlaces, setPickedPlaces] = useState([]);
 
   useEffect(() => {
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces') || [])
+    const storedPlaces = storedIds.map((id) => AVAILABLE_PLACES.find((place) => place.id === id))
+    setPickedPlaces(storedPlaces)
+  }, [])
+
+  useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
       const sortedPlaces = sortPlacesByDistance(AVAILABLE_PLACES, position.coords.latitude, position.coords.longitude)
       setSortedPlaces(sortedPlaces)
@@ -37,6 +43,11 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces') || [])
+    if (storedIds.indexOf(id) === -1) {
+      localStorage.setItem('selectedPlaces', JSON.stringify([id, ...storedIds]))
+    }
   }
 
   function handleRemovePlace() {
@@ -44,6 +55,11 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
     modal.current.close();
+
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces') || [])
+    localStorage.setItem('selectedPlaces', JSON.stringify(storedIds.filter((id) => {
+      id !== selectedPlace.current
+    })))
   }
 
   return (
